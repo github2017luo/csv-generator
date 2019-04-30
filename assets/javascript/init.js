a15f6bcdf372f2b6250e91bf810d7a0edd39f038
Lucas Werner <lwerner27@gmail.com>
2019-04-30 11:20:30 -0500

    File for initializing materlize elements.

---
+++ assets/javascript/init.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var elems = document.querySelectorAll("select");
+	var instances = M.FormSelect.init(elems, {
+		classes: "blue-text text-darken-4"
+	});
+});
